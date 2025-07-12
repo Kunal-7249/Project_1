dfa00934f70ba8631a327ce3a01fde0cba621a16
Kunal Shidurkar <kunalshidurkar@gmail.com>
2025-07-12 14:49:46 +0530

    Update MongoDB connection and deployment config

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User = require('./models/user');
 const helmet = require('helmet');
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || process.env.DB_URL_Local;
 
 
 mongoose.connect(dbUrl, {
